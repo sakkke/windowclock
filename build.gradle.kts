77603255ce42e536b36d8dac185a741a28e64944
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-30 15:29:55 +0900

    chore: update group and version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     kotlin("jvm") version "1.9.22"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "dev.sakkke"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
